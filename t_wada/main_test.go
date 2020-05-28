b44b95ff85525303ac3ac0541c8fa896c21a30f0
sencondly <dmzsecondly@gmail.com>
2020-05-28 13:41:01 +0900

    fix: テストの対称性とテストコードを整理する

--- t_wada/main_test.go
+++ t_wada/main_test.go
@@ -14,34 +14,60 @@ func Setup() FizzBuzz {
 	return FizzBuzz{}
 }
 
-// 2. 具体化させたものでテストを作る
-func Test_1を渡したら文字列1を返す(t *testing.T) {
-	fizzbuzz := Setup()
-	if "1" != fizzbuzz.Convert(1) {
-		t.Error("まともじゃないよ！")
+// 6. 実装レベルになっているので、より仕様が分かる形にする
+func Test_その他場合のテスト(t *testing.T) {
+	data := []struct {
+		name   string
+		expect string
+		val    int
+	}{
+		{name: "1を渡したら文字列1を返す", expect: "1", val: 1},
+	}
+
+	for _, line := range data {
+		t.Run(line.name, func(t *testing.T) {
+			fizzbuzz := Setup()
+			if line.expect != fizzbuzz.Convert(line.val) {
+				t.Error("まともじゃないよ！")
+			}
+		})
 	}
 }
 
-// 3. アサーションルーレットパターンを避けて別のテストとして実装
-func Test_2を渡したら文字列2を返す(t *testing.T) {
-	fizzbuzz := Setup()
-	if "2" != fizzbuzz.Convert(2) {
-		t.Error("まともじゃないよ！")
+func Test_3の倍数のテスト(t *testing.T) {
+	data := []struct {
+		name   string
+		expect string
+		val    int
+	}{
+		{name: "3を渡したら文字列Fizzを返す", expect: "Fizz", val: 3},
+	}
+
+	for _, line := range data {
+		t.Run(line.name, func(t *testing.T) {
+			fizzbuzz := Setup()
+			if line.expect != fizzbuzz.Convert(line.val) {
+				t.Error("まともじゃないよ！")
+			}
+		})
 	}
 }
 
-// 4. 先に進む
-func Test_3を渡したら文字列Fizzを返す(t *testing.T) {
-	fizzbuzz := Setup()
-	if "Fizz" != fizzbuzz.Convert(3) {
-		t.Error("まともじゃないよ！")
+func Test_5の倍数のテスト(t *testing.T) {
+	data := []struct {
+		name   string
+		expect string
+		val    int
+	}{
+		{name: "5を渡したら文字列Buzzを返す", expect: "Buzz", val: 5},
 	}
-}
 
-// 4. 先に進む
-func Test_5を渡したら文字列Buzzを返す(t *testing.T) {
-	fizzbuzz := Setup()
-	if "Buzz" != fizzbuzz.Convert(5) {
-		t.Error("まともじゃないよ！")
+	for _, line := range data {
+		t.Run(line.name, func(t *testing.T) {
+			fizzbuzz := Setup()
+			if line.expect != fizzbuzz.Convert(line.val) {
+				t.Error("まともじゃないよ！")
+			}
+		})
 	}
 }
