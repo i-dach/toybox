0495dd99037e02c5bee4090e7046501688acfa60
sencondly <dmzsecondly@gmail.com>
2021-01-26 16:19:25 +0900

    update: [oraily] template read & run - chat

--- go_tools/gotest/chat/main.go
+++ go_tools/gotest/chat/main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	if err := t.templ.Execute(w, nil); err != nil {
+		log.Fatal("ServeHttp:", err)
+	}
+}
 
 func main() {
 	// Chat main stream
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					<p>チャットしよう</p>
-				</body>
-			</html>
-		`))
-	})
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// Start web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
